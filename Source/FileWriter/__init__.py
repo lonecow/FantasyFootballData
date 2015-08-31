5693c5424754b38fee42652f92980a32e13e2d30
Robert Bitel <robert.bitel@autoliv.com>
2015-08-31 13:55:27 -0400

    Added ability to change the style of the file to be written

---
+++ Source/FileWriter/__init__.py
@@ -0,0 +1,65 @@
+
+def WriteFullList(Data, FileName):
+    fd = open(FileName, 'w')
+
+    fd.write('Player,Position,Team,Var,Owner\n')
+    for item in Data:
+        fd.write('%s,%s,%s,%.4f,%s\n' % (item['name'], item['pos'], item['team'], item['var'], item['owner']))
+
+    fd.close()
+
+
+'''93 lines per page
+   Landscape
+   Small margins'''
+def WriteByPosition(Data, FileName):
+    def _IncrementRow(Row, Column):
+        Row += 1
+
+        if Row == 67:
+            Column += 6
+            Row = 1
+
+        return (Row, Column)
+
+    count = 0
+    lists_by_pos = {}
+    for item in Data:
+        count += 1
+
+        if item['pos'] not in lists_by_pos:
+            lists_by_pos[item['pos']] = []
+
+        item['count'] = count
+        lists_by_pos[item['pos']].append(item)
+
+
+
+    row = 1
+    column = 1
+    data = []
+    for pos in lists_by_pos:
+        if len(data) < row:
+            data.append('** %s **,,,,,,' %(pos))
+            (row, column) = _IncrementRow(row, column)
+            data.append('Count,Player,Position,Team,Var,Owner,')
+            (row, column) = _IncrementRow(row, column)
+        else:
+            data[row - 1] += ('** %s **,,,,,,' %(pos))
+            (row, column) = _IncrementRow(row, column)
+            data[row - 1] += 'Count,Player,Position,Team,Var,Owner,'
+            (row, column) = _IncrementRow(row, column)
+
+        for item in lists_by_pos[pos]:
+            
+            if len(data) < row:
+                data.append('%s,%s,%s,%s,%.2f,%s,' % (item['count'],item['name'], item['pos'], item['team'], item['var'], item['owner']))
+            else:
+                data[row - 1] += '%s,%s,%s,%s,%.2f,%s,' % (item['count'],item['name'], item['pos'], item['team'], item['var'], item['owner'])
+
+            (row, column) = _IncrementRow(row, column)
+
+    fd = open(FileName, 'w')
+    for item in data:
+        fd.write('%s\n' % (item))
+    fd.close()
--- Source/FootballData.py
+++ Source/FootballData.py
@@ -7,6 +7,7 @@ Created on Aug 22, 2015
 from EspnParser import EspnData
 
 from LeagueInfo import MAFALeagueInfo, SuhNommieNationLeagueInfo, FlexLeague, CreateFullVarList
+from FileWriter import WriteByPosition, WriteFullList
 
 if __name__ == '__main__':
     data = EspnData()
@@ -17,19 +18,13 @@ if __name__ == '__main__':
     flex_10_league = CreateFullVarList(10, data, FlexLeague())
 
 
-    files_to_write = [  (mafa_12_league, './MAFA_12_VAR_List.csv'),
-                        (mafa_14_league, './MAFA_14_VAR_List.csv'),
-                        (snn_12_league, './SNN_12_VAR_List.csv'),
-                        (flex_10_league, './FLEX_10_VAR_List.csv')]
+    files_to_write = [  (mafa_12_league, './MAFA_12_VAR_List.csv',  WriteByPosition),
+                        (mafa_14_league, './MAFA_14_VAR_List.csv',  WriteByPosition),
+                        (snn_12_league, './SNN_12_VAR_List.csv',    WriteByPosition),
+                        (flex_10_league, './FLEX_10_VAR_List.csv',  WriteFullList)]
 
 
-    for (data, file_name) in files_to_write:
-        fd = open(file_name, 'w')
-
-        fd.write('Player,Position,Team,Var,Owner\n')
-        for item in data:
-            fd.write('%s,%s,%s,%.4f,%s\n' % (item['name'], item['pos'], item['team'], item['var'], item['owner']))
-
-        fd.close()
+    for (data, file_name, write_func) in files_to_write:
+        write_func(data, file_name)
 
     
