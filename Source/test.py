9e77dc452cfb518d9ffc972de798ec2100175ff7
Robert Bitel <robert.bitel@autoliv.com>
2017-08-23 14:07:43 -0400

    Added test script for testing current roster

---
+++ Source/test.py
@@ -0,0 +1,14 @@
+'''
+Created on Aug 22, 2017
+
+@author: robert.bitel
+'''
+
+from LeagueInfo import SuhNommieNationLeagueInfo
+
+
+
+
+if __name__ == '__main__':
+    snn = SuhNommieNationLeagueInfo()
+    snn.GetLeaguePlayerOweners()
