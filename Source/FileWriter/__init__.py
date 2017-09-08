9697ec5f4ecf5de457b9cd4aa5065510012d4370
Robert Bitel <lonecow.gmail.com>
2017-09-08 17:11:27 -0400

    Updated File writing program

--- Source/FileWriter/__init__.py
+++ Source/FileWriter/__init__.py
@@ -1,13 +1,74 @@
 
-def WriteFullList(Data, FileName):
-    fd = open(FileName, 'w')
+'''93 lines per page
+   Landscape
+   Small margins'''
+def WriteFullList_NoOwner(Data, FileName):
+    def _IncrementRow(Row, Column):
+        Row += 1
 
-    fd.write('Player,Position,Team,Var,Owner\n')
+        if Row == 70:
+            Column += 6
+            Row = 1
+
+        return (Row, Column)
+
+    row = 1
+    column = 1
+    data = []
     for item in Data:
-        fd.write('%s,%s,%s,%.4f,%s\n' % (item['name'], item['pos'], item['team'], float(item['var']), item['owner']))
+        if row == 1:
+            if len(data) < row:
+                data.append('Player,Position,Team,Var,')
+            else:
+                data[row -1] += 'Player,Position,Team,Var,'
 
+            (row, column) = _IncrementRow(row, column)
+
+        if len(data) < row:
+            data.append('%s,%s,%s,%.2f,' % (item['name'], item['pos'], item['team'], float(item['var'])))
+        else:
+            data[row - 1] += '%s,%s,%s,%.2f,' % (item['name'], item['pos'], item['team'], float(item['var']))
+
+        (row, column) = _IncrementRow(row, column)
+
+    fd = open(FileName, 'w')
+    for item in data:
+        fd.write('%s\n' % (item))
     fd.close()
 
+def WriteFullList_Owner(Data, FileName):
+    def _IncrementRow(Row, Column):
+        Row += 1
+
+        if Row == 70:
+            Column += 6
+            Row = 1
+
+        return (Row, Column)
+
+    row = 1
+    column = 1
+    data = []
+    for item in Data:
+        if row == 1:
+            if len(data) < row:
+                data.append('Player,Position,Team,Var,Owner,')
+            else:
+                data[row -1] += 'Player,Position,Team,Var,Owner,'
+
+            (row, column) = _IncrementRow(row, column)
+
+        if len(data) < row:
+            data.append('%s,%s,%s,%.2f,%s,' % (item['name'], item['pos'], item['team'], float(item['var']), item['owner']))
+        else:
+            data[row - 1] += '%s,%s,%s,%.2f,%s,' % (item['name'], item['pos'], item['team'], float(item['var']), item['owner'])
+
+        (row, column) = _IncrementRow(row, column)
+
+    fd = open(FileName, 'w')
+    for item in data:
+        fd.write('%s\n' % (item))
+    fd.close()
 
 '''93 lines per page
    Landscape
--- Source/FootballData.py
+++ Source/FootballData.py
@@ -5,8 +5,8 @@ Created on Aug 22, 2015
 '''
 import DataStore
 
-from LeagueInfo import MAFALeagueInfo, MAFALeagueInfo_Playoffs, SuhNommieNationLeagueInfo, FlexLeague, AutoLeagueInfo, CreateFullVarList
-from FileWriter import WriteByPosition, WriteFullList
+from LeagueInfo import MAFALeagueInfo, MAFALeagueInfo_Playoffs, SuhNommieNationLeagueInfo, FlexLeague, AutoLeagueInfo, CreateFullVarList, CreateFullOwnerList
+from FileWriter import WriteByPosition, WriteFullList_NoOwner, WriteFullList_Owner
 
 if __name__ == '__main__':
     data = DataStore.GetPlayerData()
@@ -16,24 +16,26 @@ if __name__ == '__main__':
     mafa_14_league = CreateFullVarList(14, data, MAFALeagueInfo())
     mafa_14_playoff_league = CreateFullVarList(14, data, MAFALeagueInfo_Playoffs())
     snn_12_league = CreateFullVarList(12, data, SuhNommieNationLeagueInfo())
+    snn_12_league_myteam = CreateFullOwnerList('Strk', 12, data, SuhNommieNationLeagueInfo())
     flex_10_league = CreateFullVarList(10, data, FlexLeague())
     flex_12_league = CreateFullVarList(12, data, FlexLeague())
-    AutoLeague_league = CreateFullVarList(10, data, AutoLeagueInfo())
+    AutoLeague_league = CreateFullVarList(12, data, AutoLeagueInfo())
 
 
     files_to_write = [  (mafa_10_league, './MAFA_10_VAR_List.csv',  WriteByPosition),
                         (mafa_12_league, './MAFA_12_VAR_List.csv',  WriteByPosition),
                         (mafa_14_league, './MAFA_14_VAR_List.csv',  WriteByPosition),
                         (mafa_14_playoff_league, './MAFA_14_VAR_PLAYOFF_List.csv',  WriteByPosition),
-                        (mafa_14_league, './MAFA_14_VAR_Full_List.csv',  WriteFullList),
+                        (mafa_14_league, './MAFA_14_VAR_Full_List.csv',  WriteFullList_NoOwner),
                         (snn_12_league, './SNN_12_VAR_List.csv',    WriteByPosition),
-                        (snn_12_league, './SNN_12_VAR_Full_List.csv',    WriteFullList),
+                        (snn_12_league, './SNN_12_VAR_Full_List.csv',    WriteFullList_Owner),
                         (AutoLeague_league, './AL_12_VAR_List.csv',    WriteByPosition),
-                        (flex_10_league, './FLEX_10_VAR_List.csv',  WriteFullList),
-                        (flex_12_league, './FLEX_12_VAR_List.csv',  WriteFullList)]
+                        (snn_12_league_myteam, './SNN_12_VAR_Full_List_MyTeam.csv',    WriteFullList_Owner),
+                        (flex_10_league, './FLEX_10_VAR_List.csv',  WriteFullList_NoOwner),
+                        (flex_12_league, './FLEX_12_VAR_List.csv',  WriteFullList_NoOwner)]
 
 
     for (data, file_name, write_func) in files_to_write:
         write_func(data, file_name)
 
-    
+
